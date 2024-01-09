9f88762ea28217d646a9ef5f99e50c248e38630f
Alexander Swearingen <alexander.swearingen24@gmail.com>
2024-01-08 16:06:17 -0800

    test

--- pass_check.py
+++ pass_check.py
@@ -2,11 +2,14 @@ import requests
 import hashlib
 import sys
 
+
 def api(pas):
     res = requests.get('https://api.pwnedpasswords.com/range/' + pas)
     if res.status_code != 200:
-        raise RuntimeError(f'Error Fetching {res.status_code}, check the api and try again')
+        raise RuntimeError(f'Error Fetching {
+                           res.status_code}, check the api and try again')
     return res
+
 
 def leak_pass_check(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
@@ -15,11 +18,13 @@ def leak_pass_check(hashes, hash_to_check):
             return count
     return 0
 
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first_5char, tail = sha1password[:5], sha1password[5:]
     response = api(first_5char)
     return leak_pass_check(response, tail)
+
 
 def main(args):
     for password in args:
@@ -30,5 +35,9 @@ def main(args):
             print(f'Password: {password} was not found')
     return 'Done'
 
+
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
+
+
+hello
